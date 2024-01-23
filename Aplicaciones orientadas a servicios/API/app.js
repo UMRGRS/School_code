6c5184a151cf3ea2ce40fb983066f6126f59f75c
UMRGRS <115056186+UMRGRS@users.noreply.github.com>
2024-01-22 19:49:39 -0600

    CRUD done

--- Aplicaciones orientadas a servicios/API/app.js
+++ Aplicaciones orientadas a servicios/API/app.js
@@ -5,19 +5,38 @@ const port = 3000
 
 let array=[1,2,3,4]
 
+app.get('/',(req,res)=> {
+    res.statusCode = 200
+    res.send(array.toString())
+})
 
-app.get('/:id',function(req,res){
-    const id = req.params.id
+app.put('/:valor',(req,res)=>{
+    array.push(req.params.valor)
+    res.send('Valor '+req.params.valor+' agregado')
+})
+
+app.post('/',(req,res)=>{
+    array[req.query.index] = req.query.valor
+    res.send('Indice actualizado')
+})
+
+app.delete('/:index',(req,res)=>{
+    array.splice(req.params.index,1)
+    res.send('Valor en el indice '+ req.params.index + ' eliminado')
+})
+
+app.get('/:index',(req,res)=>{
+    const index = req.params.index
     if(id>array.length-1){
         res.json({"Elemento":"No existe el elemento"})
     }
     else{
-        res.json({"Elemento":array[id]})
+        res.json({"Elemento":array[index]})
     }
 })
 
 
-app.get('/', (req, res) => res.json("ejemplo"))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
