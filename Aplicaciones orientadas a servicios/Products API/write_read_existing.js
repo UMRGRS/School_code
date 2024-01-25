8ee4ff005b5391a3b77116a5b9206f853ce73a42
UMRGRS <115056186+UMRGRS@users.noreply.github.com>
2024-01-25 17:14:02 -0600

    Adding some comments

--- Aplicaciones orientadas a servicios/Products API/write_read_existing.js
+++ Aplicaciones orientadas a servicios/Products API/write_read_existing.js
@@ -1,9 +1,14 @@
+//Importar el modulo para manejo de archivos
 var fs = require('fs')
 
+//Leemos el archivo ya existente
 var arch = fs.readFileSync('test.json')
 
+//Lo convertimos a un objeto de javascript
 data = JSON.parse(arch)
 
+//Creamos el diccionario de prueba
+//Esto se convertirá en json
 var data3 = {
     "ID_producto_3":{
         "SKU":"Código de 6 cifras",
@@ -24,10 +29,14 @@ var data3 = {
     }
 }
 
+//Accedemos a la lista en la key productos y agregamos ambos diccionarios
+//El mismo proceso del primer archivo
 data["productos"].push(data3)
 
+//Para guardar los datos primero hay que convertir nuestro objeto en json
 jsonData = JSON.stringify(data,null,4)
 
+//Writefile crea un nuevo archivo si no existe o sobrescribe el mismo si ya existe
 fs.writeFile('test.json', jsonData, 'utf8',function(err) {
     if (err) {
         console.log(err);
